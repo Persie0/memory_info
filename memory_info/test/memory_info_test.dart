848ab7f3705a15ecaa1b1f83f91c14d745927ef4
Persie0 <76871553+Persie0@users.noreply.github.com>
2026-09-24 17:40:25 +0200

    test: require macOS privacy manifest

--- memory_info/test/memory_info_test.dart
+++ memory_info/test/memory_info_test.dart
@@ -1,6 +1,23 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:io';
 
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('macOS plugin bundles its disk-space privacy manifest', () {
+    final podspec = File('macos/memory_info.podspec').readAsStringSync();
+    final manifest = File('macos/Resources/PrivacyInfo.xcprivacy');
+
+    expect(
+      podspec,
+      contains(
+        "s.resource_bundles = {'memory_info_privacy' => ['Resources/PrivacyInfo.xcprivacy']}",
+      ),
+    );
+    expect(manifest.existsSync(), isTrue);
+    final contents = manifest.readAsStringSync();
+    expect(contents, contains('NSPrivacyAccessedAPICategoryDiskSpace'));
+    expect(contents, contains('85F4.1'));
+  });
 }
